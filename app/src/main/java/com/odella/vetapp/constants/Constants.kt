cee9e1413aebad05775315594746dd029d04a56f
pogoplox <felipeoxandabarat@gmail.com>
2019-08-20 15:15:22 -0300

    login

--- app/src/main/java/com/odella/vetapp/constants/Constants.kt
+++ app/src/main/java/com/odella/vetapp/constants/Constants.kt
@@ -1,3 +1,13 @@
 package com.odella.vetapp.constants
 
-const val BASE_URL = "https://localhost/5001/api"
+const val BASE_URL = "https://localhost/5001/api/"
+
+
+open class TokenSingelton(){
+    companion object  {
+        var actualToken:String
+        init {
+            actualToken=""
+        }
+    }
+}
