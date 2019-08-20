4ffd08371237af546257a2f6cae40d9fa9c1885d
pogoplox <felipeoxandabarat@gmail.com>
2019-08-20 13:51:06 -0300

    Meme

---
+++ app/src/main/java/com/odella/vetapp/controller/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.odella.vetapp.controller
+
+import androidx.lifecycle.ViewModel
+
+
+class LoginViewModel : ViewModel() {
+    var passwordVisible = false
+}
--- app/src/main/java/com/odella/vetapp/controller/MainActivity.kt
+++ app/src/main/java/com/odella/vetapp/controller/MainActivity.kt
@@ -1,13 +1,20 @@
-package com.odella.vetapp
+package com.odella.vetapp.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.odella.vetapp.service.NetworkService
+import com.odella.vetapp.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login()
+    }
+
+    fun login(){
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
