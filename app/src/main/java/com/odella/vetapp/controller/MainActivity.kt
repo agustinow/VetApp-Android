9aea65fedc16bfe81f60749c13953ad5c7376cfe
Agustin <agustinodella@gmail.com>
2019-08-20 15:16:45 -0300

    Network service and models created

--- app/src/main/java/com/odella/vetapp/controller/MainActivity.kt
+++ app/src/main/java/com/odella/vetapp/controller/MainActivity.kt
@@ -3,14 +3,44 @@ package com.odella.vetapp.controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.afollestad.materialdialogs.DialogCallback
+import com.afollestad.materialdialogs.MaterialDialog
 import com.odella.vetapp.R
+import com.odella.vetapp.service.NetworkService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var networkService: NetworkService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        login()
+        networkService = NetworkService.create()
+        checkConnection()
+    }
+
+    fun checkConnection(){
+        networkService.checkConnection().enqueue(object: Callback<Any> {
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                MaterialDialog(this@MainActivity).show{
+                    title(R.string.network_error)
+                    positiveButton(R.string.try_again) {
+                        it.dismiss()
+                        checkConnection()
+                    }
+                    negativeButton(R.string.close) {
+                        this@MainActivity.finish()
+                    }
+                }
+            }
+
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                login()
+            }
+
+        })
     }
 
     fun login(){
