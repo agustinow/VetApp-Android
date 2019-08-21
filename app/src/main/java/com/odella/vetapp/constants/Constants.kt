bff5174320b429f0b3a95d93c1a759b54aa8abf2
agustinow <agustinodella@gmail.com>
2019-08-21 17:14:33 -0300

    Remember me is now encrypted (FUCKING SHIT)

--- app/src/main/java/com/odella/vetapp/constants/Constants.kt
+++ app/src/main/java/com/odella/vetapp/constants/Constants.kt
@@ -3,7 +3,11 @@ package com.odella.vetapp.constants
 const val BASE_URL = "https://localhost:5001/api/"
 const val PREFS_NAME = "VetAppPrefs"
 const val PREFS_USERNAME = "username"
+const val PREFS_IV = "iv"
 const val PREFS_PASSWORD = "password"
+const val TRANSFORMATION = "AES/GCM/NoPadding"
+const val ANDROID_KEYSTORE = "AndroidKeyStore"
+const val DEFAULT_IV = "elodelllajue"
 
 open class TokenSingleton{
     companion object  {
