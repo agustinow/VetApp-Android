83cf27c05110aacac6b601651b3172425cd91380
agustinow <agustinodella@gmail.com>
2019-08-21 17:14:33 -0300

    Remember me is now encrypted (FUCKING SHIT)

--- app/src/main/java/com/odella/vetapp/controller/LoginActivity.kt
+++ app/src/main/java/com/odella/vetapp/controller/LoginActivity.kt
@@ -4,14 +4,15 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.text.method.PasswordTransformationMethod
+import android.util.Base64
 import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
 import androidx.lifecycle.ViewModelProviders
 import com.odella.vetapp.R
-import com.odella.vetapp.constants.PREFS_NAME
-import com.odella.vetapp.constants.PREFS_USERNAME
-import com.odella.vetapp.constants.TokenSingleton
+import com.odella.vetapp.constants.*
 import com.odella.vetapp.service.NetworkService
 import kotlinx.android.synthetic.main.activity_login.*
 import okhttp3.MediaType
@@ -20,22 +21,117 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.GCMParameterSpec
+import java.security.SecureRandom
+import javax.crypto.spec.IvParameterSpec
+
 
 class LoginActivity : AppCompatActivity() {
     lateinit var model: LoginViewModel
+
+
+    @ExperimentalStdlibApi
+    private fun encryptAndSave(userIn: String){
+
+        if(userIn.isNotEmpty()) {
+
+            val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE)
+            keyStore.load(null)
+
+            if (!keyStore.containsAlias(PREFS_USERNAME)){
+                val keyGenerator =
+                    KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE)
+                val keySpecs = KeyGenParameterSpec.Builder(
+                    PREFS_USERNAME,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                    .build()
+                keyGenerator.init(keySpecs)
+                keyGenerator.generateKey()
+            }
+
+            val cipher = Cipher.getInstance(TRANSFORMATION)
+            /*
+            val iv = ByteArray(12)
+            val e = SecureRandom()
+            e.nextBytes(iv)
+            */
+
+            val key = keyStore.getKey(PREFS_USERNAME, null)
+            //val gcmParams = GCMParameterSpec(128, iv)
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            val bytesOfUser = userIn.encodeToByteArray()
+            var encodedBytes = cipher.doFinal(bytesOfUser)
+            var encryptedEncodedBytes = Base64.encodeToString(encodedBytes, Base64.NO_PADDING)
+
+
+            getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .putString(PREFS_USERNAME, encryptedEncodedBytes)
+                .putString(PREFS_IV, Base64.encodeToString(cipher.iv
+                    , Base64.DEFAULT))
+                .apply()
+        } else{
+            getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .putString(PREFS_USERNAME, "")
+                .putString(PREFS_IV, "")
+                .apply()
+        }
+
+    }
+
+    @ExperimentalStdlibApi
+    private fun decryptUsername() : String{
+        val iv = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(PREFS_IV, "")
+        val encrypted = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(PREFS_USERNAME, "")
+
+        if(encrypted.isNullOrEmpty()) return ""
+
+        val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE)
+        keyStore.load(null)
+        if(!keyStore.containsAlias(PREFS_USERNAME)) return ""
+
+        val key = keyStore.getKey(PREFS_USERNAME, null)
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(128, Base64.decode(iv, Base64.DEFAULT)))
+        val decodedBytes = cipher.doFinal(Base64.decode(encrypted, Base64.NO_PADDING))
+        return decodedBytes.decodeToString()
+/*
+        val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE)
+        keyStore.load(null)
+        val secretKeyEntry = keyStore.getEntry(PREFS_USERNAME, null) as KeyStore.SecretKeyEntry
+        val secretKey = secretKeyEntry.secretKey
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val spec = GCMParameterSpec(128, Base64.decode(iv, Base64.DEFAULT))
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+        val username = cipher.doFinal(Base64.decode(encrypted, Base64.DEFAULT))
+        return Base64.encodeToString(username, Base64.DEFAULT)
+ */
+    }
+
+
+    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         model = ViewModelProviders.of(this@LoginActivity)[LoginViewModel::class.java]
 
-        val pref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(PREFS_USERNAME, "")
-        if(pref.isNullOrEmpty()){
+        //val username = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(PREFS_USERNAME, "")
+        val username = decryptUsername()
+
+
+        if(username.isNullOrEmpty()){
             model.rememberMe = false
             chkRememberMe.isChecked = false
         } else {
             model.rememberMe = true
             chkRememberMe.isChecked = true
-            txtUsername.setText(pref)
+            txtUsername.setText(username)
             txtPassword.requestFocus()
         }
 
@@ -65,7 +161,8 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun tryToConnect(username:String,password:String){
+    @ExperimentalStdlibApi
+    fun tryToConnect(username:String, password:String){
         val request = JSONObject()
         request.put("Username", username)
         request.put("Password", password)
@@ -81,7 +178,7 @@ class LoginActivity : AppCompatActivity() {
                     TokenSingleton.actualToken = "Bearer "  + response.body().toString()
                     login()
                 } else {
-                    makeText(this@LoginActivity, "Invalid user", LENGTH_SHORT).show()
+                    makeText(this@LoginActivity, "Invalid credentials", LENGTH_SHORT).show()
                     txtUsername.setText("")
                     txtPassword.setText("")
                 }
@@ -91,18 +188,13 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    @ExperimentalStdlibApi
     fun login(){
         makeText(this@LoginActivity, "Good", LENGTH_SHORT).show()
         if(chkRememberMe.isChecked) {
-            getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-                .edit()
-                .putString(PREFS_USERNAME, model.username)
-                .apply()
+            encryptAndSave(txtUsername.text.toString())
         } else {
-            getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-                .edit()
-                .putString(PREFS_USERNAME, "")
-                .apply()
+            encryptAndSave("")
         }
         //val intent = Intent(this@LoginActivity, ClientMainActivity::class.java)
         //startActivity(intent)
