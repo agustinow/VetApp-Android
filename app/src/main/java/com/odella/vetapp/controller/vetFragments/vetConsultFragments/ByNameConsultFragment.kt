8dc032880a93cca29e2de43773434655c46c6e5f
pogoplox <felipeoxandabarat@gmail.com>
2019-08-28 14:45:22 -0300

    memeeees

--- app/src/main/java/com/odella/vetapp/controller/vetFragments/vetConsultFragments/ByNameConsultFragment.kt
+++ app/src/main/java/com/odella/vetapp/controller/vetFragments/vetConsultFragments/ByNameConsultFragment.kt
@@ -81,6 +81,11 @@ class ByNameConsultFragment : Fragment() {
     fun changeToConsultList(pet: Pet){
         conAdapter = ConsultsAdapter(context!!, SEE_NOTHING){
             //OPEN INFO
+            model.idConsult= it.id!!
+            var frag:Fragment=ViewConsultFragment.newInstance()
+            val ft = parentFragment!!.fragmentManager!!.beginTransaction()
+            ft.replace(R.id.activiy_vet_content, frag, frag!!.tag)
+            ft.commit()
         }
         val preFilteredConsults= model.consultByDateList!!.toList()
         var finalConsults = mutableListOf<Consult>()
