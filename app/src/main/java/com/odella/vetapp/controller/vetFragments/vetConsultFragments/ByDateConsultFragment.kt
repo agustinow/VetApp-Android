8d9f5c9f177a5e292804472d2e0f25eb2b3cd8e8
agustinow <agustinodella@gmail.com>
2019-08-29 10:18:21 -0300

    Added filters to members fragment

--- app/src/main/java/com/odella/vetapp/controller/vetFragments/vetConsultFragments/ByDateConsultFragment.kt
+++ app/src/main/java/com/odella/vetapp/controller/vetFragments/vetConsultFragments/ByDateConsultFragment.kt
@@ -1,33 +1,23 @@
 package com.odella.vetapp.controller.vetFragments.vetConsultFragments
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 import com.odella.vetapp.R
 import com.odella.vetapp.adapters.ConsultsAdapter
-import com.odella.vetapp.adapters.PetsAdapter
 import com.odella.vetapp.constants.SEE_ALL_NAMES
 import com.odella.vetapp.constants.SEE_ONLY_PET
 import com.odella.vetapp.constants.STATUS_FINISHED
 import com.odella.vetapp.constants.UserSingleton
 import com.odella.vetapp.controller.vetFragments.VetViewModel
-import com.odella.vetapp.model.Consult
-import com.odella.vetapp.service.NetworkService
 import kotlinx.android.synthetic.main.fragment_by_date_consult.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ByDateConsultFragment : Fragment() {
     lateinit var adapter: ConsultsAdapter
@@ -64,7 +54,6 @@ class ByDateConsultFragment : Fragment() {
         adapter = when(UserSingleton.userType!!){
             "vet" -> ConsultsAdapter(context!!, SEE_ONLY_PET) {
                 model.idConsult= it.id!!
-                //llamar fragmento ViewConsultFragment
                 var frag:Fragment=ViewConsultFragment.newInstance()
                 val ft = parentFragment!!.fragmentManager!!.beginTransaction()
                 ft.replace(R.id.activiy_vet_content, frag, frag!!.tag)
@@ -77,20 +66,19 @@ class ByDateConsultFragment : Fragment() {
         model.consultByDateList
         adapter.setItems(model.consultByDateList!!.toList())
 
-
         view!!.txtNoData.visibility = View.GONE
         view!!.fragment_by_date_consult_recycler.visibility = View.VISIBLE
         view!!.fragment_by_date_consult_recycler.adapter = adapter
         view!!.fragment_by_date_consult_recycler.layoutManager = LinearLayoutManager(context!!, LinearLayoutManager.VERTICAL, false)
         view!!.fragment_by_date_consult_search.visibility = View.VISIBLE
         view!!.fragment_by_date_consult_search.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                adapter.filterByName(query)
+            override fun onQueryTextSubmit(t: String?): Boolean {
+                adapter.filterByName(t)
                 return false
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                adapter.filterByName(newText)
+            override fun onQueryTextChange(t: String?): Boolean {
+                adapter.filterByName(t)
                 return false
             }
         })
